f7648541e4b40d7f528c1ca1026500eaa74c4851
SepehrGanji <sepehrganji79@gmail.com>
2023-07-08 18:31:23 +0330

    remove redundant comments

--- src/plugins/support.ts
+++ src/plugins/support.ts
@@ -1,19 +1,15 @@
 import fp from 'fastify-plugin';
 
-export interface SupportPluginOptions {
-  // eslint-disable-line
-  // Specify Support plugin options here
-}
+/* eslint-disable */
+export interface SupportPluginOptions {}
+/* eslint-enable */
 
-// The use of fastify-plugin is required to be able
-// to export the decorators to the outer scope
 export default fp<SupportPluginOptions>(async (fastify, opts) => {
   fastify.decorate('someSupport', function () {
     return 'hugs';
   });
 });
 
-// When using .decorate you have to specify added properties for Typescript
 declare module 'fastify' {
   export interface FastifyInstance {
     someSupport(): string;
