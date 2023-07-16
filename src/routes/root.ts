9d8cee9bcc42a8cc11e31ca6ad4d9344fc3ca4aa
Fatemeh-Abdollahi <f.abdollahi.yazdi@gmail.com>
2023-07-16 13:28:00 +0330

    Add CoinGecko APIs

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,8 +1,46 @@
+const fetch = require('node-fetch');
 import { FastifyPluginAsync } from 'fastify';
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
     return { root: true };
+  });
+
+  ///we can use one of the fetchers
+  //number1
+  fastify.get('/fetcher1', async function (request, reply) {
+    try {
+      const coin = 'bitcoin';
+      const vsCurrencies: string[] = ['usd', 'eur', 'gbp'];
+
+      const url = `https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=${vsCurrencies.join(',')}`;
+      const response = await fetch(url);
+      const data = await response.json();
+
+      return { data }; // Return the fetched data as the response
+    } catch (error) {
+      console.log('An error occurred:', error);
+      return { error: 'An error occurred' }; // Return an error response if an error occurs
+    }
+  });
+
+  //number2
+  fastify.get('/fetcher2', async function (request, reply) {
+    try {
+      const coinId = 'bitcoin'; // Specify the coin ID or symbol we want to fetch
+      const url = `https://api.coingecko.com/api/v3/coins/${coinId}`;
+  
+      const response = await fetch(url);
+      const data = await response.json();
+      
+      // Extract the desired data, such as price, from the response
+      const price = data.market_data.current_price.usd;
+  
+      return { price }; // Return the fetched data as the response
+    } catch (error) {
+      console.log('An error occurred:', error);
+      return { error: 'An error occurred' }; // Return an error response if an error occurs
+    }
   });
 };
 
--- test/routes/root.test.ts
+++ test/routes/root.test.ts
@@ -3,9 +3,36 @@ import { build } from '../helper';
 
 test('default root route', async (t) => {
   const app = await build(t);
-
+  //root
   const res = await app.inject({
     url: '/',
   });
   t.same(JSON.parse(res.payload), { root: true });
+
+  //fetcher1
+  const res1 = await app.inject({
+    url: '/fetcher1',
+  });
+  const expectedDataStructure1 = {
+    data: {
+      bitcoin: {
+        usd: Number,
+        eur: Number,
+        gbp: Number
+      }
+    }
+  };
+
+  // Parse the response payload
+  const responseData1 = JSON.parse(res1.payload);
+
+  // Check if the response data has the same structure as the expected structure
+  t.match(responseData1, expectedDataStructure1);
+
+  //fetcher2
+  const res2 = await app.inject({
+    url: '/fetcher2',
+  });
+  // Parse the response payload
+  t.match(JSON.parse(res2.payload), { price: Number });
 });
