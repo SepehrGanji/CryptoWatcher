739fbd404fae681a91bca8bc83600fca90175c89
Fatemeh-Abdollahi <f.abdollahi.yazdi@gmail.com>
2023-08-04 16:47:01 +0330

    add knexfile

---
+++ test/knexfile.ts
@@ -0,0 +1,12 @@
+// test/knexfile.ts
+import { Knex } from 'knex';
+
+const config: Knex.Config = {
+  client: 'sqlite3',
+  connection: {
+    filename: './test/test.db',
+  },
+  useNullAsDefault: true,
+};
+
+export default config;
