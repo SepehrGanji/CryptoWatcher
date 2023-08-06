a7c97b3d25381936e6fb9194713314366186467d
Fatemeh-Abdollahi <f.abdollahi.yazdi@gmail.com>
2023-08-06 17:53:22 +0330

    fix migrations

--- test/migrations/20230806141555_sqlite.ts
+++ test/migrations/20230806141555_sqlite.ts
@@ -1,10 +1,14 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('name').notNullable();
+    table.string('email').notNullable();
+    table.timestamps(true, true);
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists('users');
 }
-
